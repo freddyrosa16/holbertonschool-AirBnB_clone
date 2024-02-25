f7dfd43be265812bdaac7bcd1fad99835531026a
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-25 13:48:44 -0400

    Fixing save and to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel():
         and update timestamp. '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         ''' Returns a string representation of the BaseModel instance. '''
@@ -20,12 +20,12 @@ class BaseModel():
 
     def save(self):
         ''' Updates the 'updated_at' attribute with the current datetime. '''
-        self.save = datetime.datetime.now()
+        self.update_at = datetime.datetime.now()
 
     def to_dict(self):
         ''' Returns a dictionary representation of the BaseModel instance. '''
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['update_at'] = obj_dict['update_at'].isoformat
-        obj_dict['created_at'] = obj_dict['created_at'].isoformat
+        obj_dict['update_at'] = self.updated_at.isoformat
+        obj_dict['created_at'] = self.created_at.isoformat
         return obj_dict
