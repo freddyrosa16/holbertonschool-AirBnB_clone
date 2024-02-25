5868c500a2cf190b263a4e6448b040609c49b2ed
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-25 14:01:17 -0400

    Fixing import datetime in my code

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Class that defines all common attributes/methods for other classes. '''
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel():
@@ -11,20 +11,20 @@ class BaseModel():
         ''' Initializes a BaseModel instance with a unique ID,
         creation timestamp,and update timestamp. '''
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         ''' Returns a string representation of the BaseModel instance. '''
-        return (f'[BaseModel] ({self.id}) {self.__dict__}')
+        return (f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
 
     def save(self):
         ''' Updates the 'updated_at' attribute with the current datetime. '''
-        self.update_at = datetime.datetime.now()
+        self.update_at = datetime.now()
 
     def to_dict(self):
         ''' Returns a dictionary representation of the BaseModel instance. '''
-        obj_dict = self.__dict__.copy()
+        obj_dict = dict(self.__dict__)
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['update_at'] = self.updated_at.isoformat()
         obj_dict['created_at'] = self.created_at.isoformat()
