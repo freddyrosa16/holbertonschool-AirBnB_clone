eb7b7bdd7d9adf4e4300946fe76c0810906b812b
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-27 12:58:41 -0400

    Fixing init method

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Class that defines all common attributes/methods for other classes. """
-import models
 import uuid
 from datetime import datetime
 
@@ -12,32 +11,35 @@ class BaseModel():
     """
 
     def __init__(self, *args, **kwargs):
-        """ Initialization of the model class. """
+        """initialization of the model class"""
         if kwargs:
             for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    time_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                elif key != '__class__':
-                    self.__dict__[key] = value
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """ Returns a string representation of BaseModel. """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the 'updated_at' attribute with the current datetime."""
+        """ Updates the 'updated_at' attribute with the current datetime. """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """ Converts the object to a dictionary representation. """
-        obj_dict = self.__dict__.copy
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
