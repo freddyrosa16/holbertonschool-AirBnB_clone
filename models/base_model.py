9991be420d695804b1236021debc821c51dce39b
freddyrosa16 <freddy.rosa16@gmail.com>
2024-02-27 11:00:46 -0400

    Fixing documentation

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-''' Class that defines all common attributes/methods for other classes. '''
+""" Class that defines all common attributes/methods for other classes. """
 import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
-    '''
+    """
     Class that defines all common attributes/methods
     for other classes.
-    '''
+    """
 
-    def __init__(self,  *args, **kwargs):
-        ''' Initializes model class. '''
+    def __init__(self, *args, **kwargs):
+        """ Initialization of the model class. """
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -32,17 +32,18 @@ class BaseModel():
             models.storage.new(self)
 
     def __str__(self):
-        ''' Returns a string representation of the BaseModel instance. '''
+        """ Returns a string representation of BaseModel. """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        ''' Updates the 'updated_at' attribute with the current datetime. '''
+        """Updates the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        ''' Returns a dictionary representation of the BaseModel instance. '''
-        dictionary = dict(self.__dict__)
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """ Converts the object to a dictionary representation. """
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
